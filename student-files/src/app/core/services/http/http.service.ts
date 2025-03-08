de70ca07caed21eb95bcbca3c4b38fc7b28660d3
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-03-08 16:56:28 +0100

    Added TODO comment

--- student-files/src/app/core/services/http/http.service.ts
+++ student-files/src/app/core/services/http/http.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class HttpService {
-  private readonly apiUrl: string = 'API_URL';
+  private readonly apiUrl: string = 'API_URL'; // TODO: To be changed with actual API url
 
   constructor(private readonly httpClient: HttpClient) {}
 }
