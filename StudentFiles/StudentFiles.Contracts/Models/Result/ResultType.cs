8448e4b2fa014b36b5b4128134b69f6f61b30d98
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-03-15 19:31:53 +0100

    Backend: Introduce Result pattern (#9)

---
+++ StudentFiles/StudentFiles.Contracts/Models/Result/ResultType.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentFiles.Contracts.Models.Result
+{
+    public enum ResultType
+    {
+        Success,
+        NotFound,
+        Unauthorized,
+        Invalid,
+        BadRequest,
+        Forbidden,
+        Conflict,
+        InternalError
+    }
+}
