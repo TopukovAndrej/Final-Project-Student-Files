bce48a0bdfed8f7269c8608c77ef45be38679bc9
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-03-15 19:26:39 +0100

    Init

---
+++ StudentFiles/StudentFiles.Api/Common/ResultTypeMapper.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentFiles.Api.Common
+{
+    using StudentFiles.Contracts.Models.Result;
+
+    public static class ResultTypeMapper
+    {
+        public static int MapToHttpStatusCode(ResultType resultType)
+        {
+            return resultType switch
+            {
+                ResultType.Success => StatusCodes.Status200OK,
+                ResultType.NotFound => StatusCodes.Status404NotFound,
+                ResultType.Unauthorized => StatusCodes.Status401Unauthorized,
+                ResultType.Invalid => StatusCodes.Status422UnprocessableEntity,
+                ResultType.BadRequest => StatusCodes.Status400BadRequest,
+                ResultType.Forbidden => StatusCodes.Status403Forbidden,
+                ResultType.Conflict => StatusCodes.Status409Conflict,
+                ResultType.InternalError => StatusCodes.Status500InternalServerError,
+                _ => StatusCodes.Status500InternalServerError,
+            };
+        }
+    }
+}
