4bb66a2df0695284d4c45a928c8c1523aa83ddbd
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-03-16 09:14:31 +0100

    Backend: Introduced Student entity (#10)

---
+++ StudentFiles/StudentFiles.Domain/Entities/Student/Student.cs
@@ -0,0 +1,45 @@
+﻿namespace StudentFiles.Domain.Entities.Student
+{
+    using StudentFiles.Domain.Common.Entities;
+
+    public class Student : AggregateRoot
+    {
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public DateOnly DateOfBirth { get; private set; }
+
+        public string IndexNumber { get; private set; }
+
+        public decimal Gpa { get; private set; }
+
+        public string PhoneNumber { get; private set; }
+
+        public int EnrollmentYear { get; private set; }
+
+        public StudentStatus Status { get; private set; }
+
+        private Student(int id,
+                        Guid uid,
+                        bool isDeleted,
+                        string firstName,
+                        string lastName,
+                        DateOnly dateOfBirth,
+                        string indexNumber,
+                        decimal gpa,
+                        string phoneNumber,
+                        int enrollmentYear,
+                        StudentStatus status) : base(id, uid, isDeleted)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            IndexNumber = indexNumber;
+            Gpa = gpa;
+            PhoneNumber = phoneNumber;
+            EnrollmentYear = enrollmentYear;
+            Status = status;
+        }
+    }
+}
