c26bf84354f7192542fbc1b8f887329c412ccdfe
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-03-29 12:29:11 +0100

    Backend: Introduced Enumeration class (#16)

---
+++ StudentFiles/StudentFiles.Contracts/Common/ErrorCodes.cs
@@ -0,0 +1,7 @@
+﻿namespace StudentFiles.Contracts.Common
+{
+    public static class ErrorCodes
+    {
+        public static readonly string UserRoleNotValid = "USER_ROLE_NOT_VALID";
+    }
+}
--- StudentFiles/StudentFiles.Domain/Entities/User/UserRole.cs
+++ StudentFiles/StudentFiles.Domain/Entities/User/UserRole.cs
@@ -1,9 +1,27 @@
 ﻿namespace StudentFiles.Domain.Entities.User
 {
-    public enum UserRole
+    using StudentFiles.Contracts.Common;
+    using StudentFiles.Contracts.Models.Result;
+    using StudentFiles.Domain.Common.Models;
+
+    public class UserRole(byte id, string code) : Enumeration(id: id, code: code)
     {
-        Admin = 0,
-        Student = 1,
-        Professor = 2
+        public static readonly UserRole Admin = new(id: 1, code: "ADMIN");
+
+        public static readonly UserRole Professor = new(id: 2, code: "PROFESSOR");
+
+        public static readonly UserRole Student = new(id: 3, code: "STUDENT");
+
+        public static Result<UserRole> Create(string code)
+        {
+            UserRole? role = GetAll<UserRole>().SingleOrDefault(predicate: x => x.Code == code);
+
+            if (role is null)
+            {
+                return Result<UserRole>.Failed(errorMessage: ErrorCodes.UserRoleNotValid, resultType: ResultType.Invalid);
+            }
+
+            return Result<UserRole>.Success(value: role);
+        }
     }
 }
