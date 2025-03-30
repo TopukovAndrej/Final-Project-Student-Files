177e50ed76a068ac3f019f4a912d6f4d83b8310c
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-03-30 18:14:20 +0200

    Added HasColumnName in EF Core configuration

--- StudentFiles/StudentFiles.Infrastructure/Common/Configurations/UserConfiguration.cs
+++ StudentFiles/StudentFiles.Infrastructure/Common/Configurations/UserConfiguration.cs
@@ -11,11 +11,11 @@
             builder.ToTable(name: "User");
 
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.Uid).HasColumnType("UNIQUEIDENTIFIER").IsRequired();
-            builder.Property(x => x.IsDeleted).HasColumnType("BIT").HasDefaultValue(false);
-            builder.Property(x => x.Username).HasColumnType("NVARCHAR").HasMaxLength(77).IsRequired();
-            builder.Property(x => x.HashedPassword).HasColumnType("NVARCHAR").HasMaxLength(60).IsRequired();
-            builder.Property(x => x.Role).HasColumnType("NVARCHAR").HasMaxLength(9).IsRequired();
+            builder.Property(x => x.Uid).HasColumnName("Uid").HasColumnType("UNIQUEIDENTIFIER").IsRequired();
+            builder.Property(x => x.IsDeleted).HasColumnName("IsDeleted").HasColumnType("BIT").HasDefaultValue(false);
+            builder.Property(x => x.Username).HasColumnName("Username").HasColumnType("NVARCHAR").HasMaxLength(77).IsRequired();
+            builder.Property(x => x.HashedPassword).HasColumnName("Password").HasColumnType("NVARCHAR").HasMaxLength(60).IsRequired();
+            builder.Property(x => x.Role).HasColumnName("Role").HasColumnType("NVARCHAR").HasMaxLength(9).IsRequired();
         }
     }
 }
