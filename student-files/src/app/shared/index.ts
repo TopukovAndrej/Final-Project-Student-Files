45fd815fc221467ffc4b524979b19e12e415b106
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-09-28 09:32:30 +0200

    UI: Introduced Base component (#27)

--- student-files/src/app/shared/index.ts
+++ student-files/src/app/shared/index.ts
@@ -5,3 +5,4 @@ export * from './components/footer/footer.component';
 export * from './utils/form-validators';
 export * from './utils/constants';
 export * from './utils/error-messages';
+export * from './components/index';
